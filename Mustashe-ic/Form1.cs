08b52b2a9644603740122cd0c38f3aa47e343f0d
Dominic <dominicacox@gmail.com>
2014-05-28 15:56:05 -0400

    Added event for start button and added buttons for game modes.

--- Mustashe-ic/Form1.cs
+++ Mustashe-ic/Form1.cs
@@ -16,5 +16,12 @@ namespace Mustashe_ic
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var f = new mainForm();
+            this.Hide();
+            f.Show();
+        }
     }
 }
--- Mustashe-ic/mainForm.Designer.cs
+++ Mustashe-ic/mainForm.Designer.cs
@@ -28,11 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(599, 800);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Story Mode";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(704, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(599, 800);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Endless Mode";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1379, 1073);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "mainForm";
             this.Text = "mainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
