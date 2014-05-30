5b4008f4c032291d22f162dd3b7af64889523a0a
Dominic <dominicacox@gmail.com>
2014-05-30 10:46:10 -0400

    Removed Form1 and changed main form to gameMain. Added event to start button that dynamically creates mode buttons. Linked worldsMode button to click event.

---
+++ Mustashe-ic/gameMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mustashe_ic
+{
+    public partial class gameMain : Form
+    {
+
+        System.Windows.Forms.Button button_worldsMode;
+        System.Windows.Forms.Button button_endlessMode;
+        public gameMain()
+        {
+            InitializeComponent();
+            gamePlay game = new gamePlay(this, 4, 1);
+
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+            button2.Hide();
+            label_GameTitleLabel.Hide();
+            
+            //Start mode button drawing
+            button_worldsMode = new Button();
+            button_worldsMode.Text = "Worlds";
+            button_worldsMode.Size = new Size(250, 390);
+            button_worldsMode.AutoSize = true;
+            button_worldsMode.Location = new Point(5, 5);
+            button_worldsMode.Click += new System.EventHandler(worldButton_Click);
+            this.Controls.Add(button_worldsMode);
+
+
+            button_endlessMode = new Button();
+            button_endlessMode.Text = "Endless";
+            button_endlessMode.Size = new Size(250, 390);
+            button_endlessMode.AutoSize = true;
+            button_endlessMode.Location = new Point(255, 5);
+            this.Controls.Add(button_endlessMode);            
+        }
+
+        private void worldButton_Click(object sender, EventArgs e) //Maybe this will be the event that starts the game or 
+        {
+
+        }
+       
+
+
+    }
+}
--- Mustashe-ic/gamePlay.cs
+++ Mustashe-ic/gamePlay.cs
@@ -8,5 +8,69 @@ namespace Mustashe_ic
 {
     class gamePlay
     {
+        tileClass[,] board;
+        int n; //nXn
+        int score;
+        public int timer { set; get; }
+        int count;
+        int randX, randY;
+        Random rand;
+        Queue<Tuple<int, int>> hiddenList;
+
+
+        public gamePlay(gameMain g, int size, int mode)
+        {
+            score = 0;
+            timer = 30;
+            n = size;
+            init_board(n, g);
+            //g.label_Score.Text = score.ToString();
+            ///g.label_timer.Text = timer.ToString();
+            randX = 0;
+            randY = 2;
+            n = (int)Math.Sqrt(n);
+            rand = new Random();
+            count = rand.Next(randX, randY); //get random tile wait time
+            hiddenList = new Queue<Tuple<int, int>>();
+
+        }
+
+        private void init_board(int size, gameMain g) //Creates a size X size grid of tiles 
+        {
+            board = new tileClass[size, size];
+
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    board[i, j] = new tileClass();
+                    board[i, j].tile.Size = new System.Drawing.Size(100, 100);
+                    board[i, j].tile.Location = new System.Drawing.Point(i * 200 + 5, j * 100 + 5);
+                    //g.panel_tileHolder.Controls.Add(board[i, j].tile);
+
+                }
+            }
+
+        }
+
+        public void gameTick()
+        {
+            count--;
+            if (hiddenList.Count >= 2)
+            {
+
+                var temp = hiddenList.Dequeue();
+                board[int.Parse(temp.Item1.ToString()), int.Parse(temp.Item2.ToString())].tile.Show();
+            }
+
+            if (count == 0)
+            {
+                int i = rand.Next(n + 1);
+                int j = rand.Next(n + 1);
+                hiddenList.Enqueue(Tuple.Create(i, j));
+                board[i, j].tile.Hide();
+                count = rand.Next(randX, randY); //get random tile wait time
+            }
+        }
     }
 }
